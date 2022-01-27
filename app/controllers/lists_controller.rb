0c54a46713884ddbb64df4026b41f8a0dd62cd1f
EC2 Default User <ec2-user@ip-172-31-1-11.ec2.internal>
2022-01-27 16:15:07 +0000

    chapter4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,12 +13,15 @@ class ListsController < ApplicationController
   end
 
   def index
+    @lists=List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+      @list = List.find(params[:id])
   end
 
 private
